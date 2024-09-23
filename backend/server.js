413ec97f351d3e399359ce546e844084e17acc93
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-09-23 19:58:41 +0300

    Part - 3 | SignUp route to'liq

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // parse JSON requests bodies
+
 app.use("/api/v1/auth", authRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   connectDB();
 });
+
