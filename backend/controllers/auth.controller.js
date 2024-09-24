d2c1ed8c741dc45c65e78e32a1f3026602cc2fb5
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-09-24 16:48:20 +0300

    Part - 3 | Mailtrap Setup va Email ga Welcome message jo'natish

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 
 export const signup = async (req, res) => {
   try {
@@ -50,7 +51,13 @@ export const signup = async (req, res) => {
 
     res.status(201).json({ message: "User registered successfully" });
 
-    // todo: send welcome email
+    const profileUrl = process.env.CLIENT_URL + "/profile/" + user.username;
+
+    try {
+      await sendWelcomeEmail(user.email, user.name, profileUrl);
+    } catch (emailError) {
+      console.error("Error in sending welcome email: ", emailError);
+    }
   } catch (error) {
     console.log("Error in signup: ", error.message);
     res.status(500).json({ message: "Internal server error" });
