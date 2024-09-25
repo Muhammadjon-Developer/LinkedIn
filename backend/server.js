26e12caa147ed52e803f0223d8dff959b5e49208
Muhammadjon-Developer <olimovmuhammadjon1605@gmail.com>
2024-09-25 16:39:51 +0300

    Part- 5 | Protect Route Middleware yaratish

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
@@ -10,12 +11,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // parse JSON requests bodies
-
 app.use("/api/v1/auth", authRoutes);
-
+app.use(cookieParser());
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   connectDB();
 });
-
